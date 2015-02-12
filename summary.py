a07cdbc4bfb2a9d3bfcd37aa2386806bc72119d4
Quentin De Coninck <quentindeco54@gmail.com>
2015-02-12 23:13:13 +0100

    [IMP] summary: nb of connections with ylabel

--- summary.py
+++ summary.py
@@ -832,6 +832,7 @@ def nb_conns_by_app(log_file=sys.stdout):
         ind = np.arange(len(to_plot_mean))
         plt.bar(ind, to_plot_mean, width, yerr=to_plot_std, ecolor="r")
         plt.xticks(ind + width / 2, xlabels_loc, rotation='vertical')
+        plt.ylabel("Total number of connections", fontsize=16)
         # Pad margins so that markers don't get clipped by the axes
         plt.margins(0.2)
         # Tweak spacing to prevent clipping of tick-labels
