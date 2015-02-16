30c41ee9480749d271cbda1c7038b000026afd94
Quentin De Coninck <quentindeco54@gmail.com>
2015-02-16 11:37:52 +0100

    [IMP] all_summary: remove outliers

--- all_summary.sh
+++ all_summary.sh
@@ -8,6 +8,6 @@ DIRS=$@
 for cond in both3 both4 rmnet3 rmnet4 wlan both4TCL5pD100m both4TCL5p both4TCL15p both4TCD10m both4TCD100m both4TCD1000m; do
     for app in dailymotion drive dropbox facebook firefox firefoxspdy messenger shazam spotify youtube; do
         echo "Summary with $cond on app $app"
-        ./summary.py -a $app -c $cond $TIME -d $DIRS
+        ./summary.py -r -a $app -c $cond $TIME -d $DIRS
     done
 done
