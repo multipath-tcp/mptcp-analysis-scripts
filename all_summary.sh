c60fd5af274d5a08a90fe78eb27e667750cb5dfc
Quentin De Coninck <quentindeco54@gmail.com>
2015-03-02 12:16:43 +0100

    [REM] all_summary: remove useless cond and apps

--- all_summary.sh
+++ all_summary.sh
@@ -5,8 +5,8 @@ TIME=$1
 shift
 DIRS=$@
 
-for cond in both3 both4 rmnet3 rmnet4 wlan both4TCL5pD100m both4TCL5p both4TCL15p both4TCD10m both4TCD100m both4TCD1000m; do
-    for app in dailymotion drive dropbox facebook firefox firefoxspdy messenger shazam spotify youtube; do
+for cond in both3 both4 rmnet3 rmnet4 wlan; do
+    for app in dailymotion drive dropbox facebook firefox firefoxspdy messenger spotify youtube; do
         echo "Summary with $cond on app $app"
         ./summary.py -a $app -c $cond $TIME -d $DIRS
     done
