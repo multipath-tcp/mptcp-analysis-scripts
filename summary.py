88ad092e7e0e2389a23f68fb51bb05f7e2b5c9eb
Quentin De Coninck <quentindeco54@gmail.com>
2015-03-16 10:53:12 +0100

    [FIX] summary: check if dictionary

--- summary.py
+++ summary.py
@@ -1680,11 +1680,11 @@ def reinject_plot(log_file=sys.stdout, min_bytes=0.0):
                         reinject_packs_d2s += flow.attr[co.REINJ_ORIG_PACKS_D2S]
                         if co.BYTES_S2D in flow.attr:
                             bytes_s2d += flow.attr[co.BYTES_S2D]
-                        elif flow.attr[co.IF] in conn.attr[co.BYTES_S2D]:
+                        elif isinstance(conn.attr[co.BYTES_S2D], dict) and flow.attr[co.IF] in conn.attr[co.BYTES_S2D]:
                             bytes_s2d += conn.attr[co.BYTES_S2D][flow.attr[co.IF]]
                         if co.BYTES_D2S in flow.attr:
                             bytes_d2s += flow.attr[co.BYTES_D2S]
-                        elif flow.attr[co.IF] in conn.attr[co.BYTES_D2S]:
+                        elif isinstance(conn.attr[co.BYTES_D2S], dict) and flow.attr[co.IF] in conn.attr[co.BYTES_D2S]:
                             bytes_d2s += conn.attr[co.BYTES_D2S][flow.attr[co.IF]]
                         packs_s2d += flow.attr[co.PACKS_S2D]
                         packs_d2s += flow.attr[co.PACKS_D2S]
