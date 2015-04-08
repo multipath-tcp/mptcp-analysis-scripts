27a09c004bb9ca76248244ace2ef346b1601f4c5
Quentin De Coninck <quentindeco54@gmail.com>
2015-04-08 16:16:11 +0200

    [IMP] common: with annotations

--- common.py
+++ common.py
@@ -809,17 +809,17 @@ def scatter_plot(data, xlabel, ylabel, color, sums_dir_exp, base_graph_name, plo
         else:
             plt.ylim(0.0, max(plt.ylim()[1], 1))
 
-        # plt.annotate('1', xy=(0.57, 0.96),  xycoords="axes fraction",
-        #         xytext=(0.85, 0.85), textcoords='axes fraction',
-        #         arrowprops=dict(facecolor='black', shrink=0.05),
-        #         horizontalalignment='right', verticalalignment='bottom', size='large'
-        #         )
-        #
-        # plt.annotate('2', xy=(0.38, 0.04),  xycoords="axes fraction",
-        #         xytext=(0.125, 0.2), textcoords='axes fraction',
-        #         arrowprops=dict(facecolor='black', shrink=0.05),
-        #         horizontalalignment='left', verticalalignment='top', size='large'
-        #         )
+        plt.annotate('1', xy=(0.57, 0.96),  xycoords="axes fraction",
+                xytext=(0.85, 0.85), textcoords='axes fraction',
+                arrowprops=dict(facecolor='black', shrink=0.05),
+                horizontalalignment='right', verticalalignment='bottom', size='large'
+                )
+
+        plt.annotate('2', xy=(0.38, 0.04),  xycoords="axes fraction",
+                xytext=(0.125, 0.2), textcoords='axes fraction',
+                arrowprops=dict(facecolor='black', shrink=0.05),
+                horizontalalignment='left', verticalalignment='top', size='large'
+                )
 
         graph_fname = base_graph_name + "_" + condition + ".pdf"
         graph_full_path = os.path.join(sums_dir_exp, graph_fname)
