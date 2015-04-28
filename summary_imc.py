4c3a24e34a92d032b04c2a1c89be7c335b6be79d
Quentin De Coninck <quentindeco54@gmail.com>
2015-04-28 13:44:45 +0200

    [IMP] summary_imc: log huge delta

--- summary_imc.py
+++ summary_imc.py
@@ -1485,6 +1485,8 @@ def delay_mpcapable_mpjoin_quantify_handover(log_file=sys.stdout, threshold_hand
                     if delta >= threshold_handover and not handover_detected:
                         handover_detected = True
                         handover_conns[fname][conn_id] = conn
+                    if delta >= 5000:
+                        print("HUGE DELTA", fname, conn_id, flow_id, file=log_file)
 
     # Do a first CDF plot of the delta between initial SYN and additional ones
     base_graph_path = os.path.join(sums_dir_exp, 'cdf_delta_addtitional_syns')
