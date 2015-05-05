180d23fe4e3a717cb7ab38106d1645d3e57d6a42
Quentin De Coninck <quentindeco54@gmail.com>
2015-05-06 00:37:06 +0200

    [IMP] summary_imc: with 1MB

--- summary_imc.py
+++ summary_imc.py
@@ -373,8 +373,8 @@ def count_unused_subflows(log_file=sys.stdout):
 
 
 def textual_summary(log_file=sys.stdout):
-    data = {'all': {'<1s': 0, ">=1s<10K": 0, ">=1s>=10K": 0, "<9s": 0, ">=9s<10K": 0, ">=9s>=10K": 0, '<10K': 0, '1B': 0, '2B': 0, '>=100s': 0, '<10s': 0}}
-    count = {'all': {'<1s': 0, ">=1s<10K": 0, ">=1s>=10K": 0, "<9s": 0, ">=9s<10K": 0, ">=9s>=10K": 0, '<10K': 0, '1B': 0, '2B': 0, '>=100s': 0, '<10s': 0}}
+    data = {'all': {'<1s': 0, ">=1s<10K": 0, ">=1s>=10K": 0, "<9s": 0, ">=9s<10K": 0, ">=9s>=10K": 0, '<10K': 0, '1B': 0, '2B': 0, '>=100s': 0, '<10s': 0, '>=1M': 0}}
+    count = {'all': {'<1s': 0, ">=1s<10K": 0, ">=1s>=10K": 0, "<9s": 0, ">=9s<10K": 0, ">=9s>=10K": 0, '<10K': 0, '1B': 0, '2B': 0, '>=100s': 0, '<10s': 0, '>=1M': 0}}
     tot_count = {'all': 0.0}
 
     for fname, conns in connections.iteritems():
@@ -422,6 +422,9 @@ def textual_summary(log_file=sys.stdout):
             elif nb_bytes_s2d + nb_bytes_d2s < 10000:
                 count['all']['<10K'] += 1
                 data['all']['<10K'] += nb_bytes_s2d + nb_bytes_d2s
+            elif nb_bytes_s2d + nb_bytes_d2s >= 1000000:
+                count['all']['>=1M'] += 1
+                data['all']['>=1M'] += nb_bytes_s2d + nb_bytes_d2s
             if duration < 9:
                 data['all']["<9s"] += nb_bytes_s2d + nb_bytes_d2s
                 count['all']["<9s"] += 1
