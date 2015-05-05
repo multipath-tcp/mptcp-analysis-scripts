0b21842a15766b044850dcaa685464d86d8e4c2a
Quentin De Coninck <quentindeco54@gmail.com>
2015-05-05 17:59:32 +0200

    [IMP] plot_acksize: see strange values

--- plot_acksize.py
+++ plot_acksize.py
@@ -100,6 +100,9 @@ for fname, acks_fname in acks[MPTCP].iteritems():
                     if int(value_ack) > 100000000:
                         print(fname, conn_id)
                         continue
+                    if int(value_ack) < -200000000:
+                        print(fname, conn_id, int(value_ack))
+                        continue
                     if int(value_ack) not in sums_acks[MPTCP][direction]:
                         sums_acks[MPTCP][direction][int(value_ack)] = int(nb_ack)
                     else:
