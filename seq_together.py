d6de570b984c3044c7319fd9d58cd28e42edcc44
Quentin De Coninck <quentindeco54@gmail.com>
2015-05-11 16:39:18 +0200

    [ADD] seq_together: one seq graph per file

--- seq_together.py
+++ seq_together.py
@@ -54,7 +54,7 @@ parser.add_argument('-S',
 parser.add_argument("-d",
                     "--dirs", help="list of directories to aggregate", nargs="+")
 parser.add_argument("-c",
-                    "--csv", help="take a list of csv files", nargs="+")
+                    "--csv", help="directory where csvs/xpls are located")
 
 args = parser.parse_args()
 
@@ -86,6 +86,123 @@ def fetch_data(dir_exp):
     return dico
 
 connections = fetch_data(stat_dir_exp)
+
+
+def seq_d2s_all_connections():
+    for fname, conns in connections.iteritems():
+        seqs = {co.WIFI: [], co.CELL: []}
+        start_connections = []
+
+        if fname.startswith('mptcp'):
+            for csv_path in glob.glob(os.path.join(csv_dir_exp, fname + '_*.csv')):
+                csv_fname = os.path.basename(csv_path)
+                # Preprocessing, avoid wasting time with not interesting files
+                from_server_to_smartphone = mptcp.is_reverse_connection(csv_fname)
+                if not from_server_to_smartphone:
+                    continue
+                conn_id = mptcp.get_connection_id(csv_fname)
+
+                # Opening of the file
+                try:
+                    csv_file = open(csv_path)
+                    data = csv_file.readlines()
+                    csv_file.close()
+                except IOError as e:
+                    print(str(e))
+                    continue
+
+                # Now process the file
+                conn = connections[fname][conn_id]
+                start_connections.append(conn.attr[co.START])
+                flow_interface = {}
+                for flow_id, flow in conn.flows.iteritems():
+                    flow_interface[flow_id] = flow.attr[co.IF]
+
+                for line in data:
+                    split_line = line.split(',')
+
+                    if int(split_line[3]) == 1:
+                        # MAP
+                        timestamp = float(split_line[0])
+                        seq_start = int(split_line[1])
+                        flow_id = int(split_line[2]) - 1
+                        # is_ack = False # int(split_line[3]) == 1
+                        seq_end = int(split_line[4])
+                        reinject_flow = int(split_line[5]) - 1 # If not negative, the flow where packet was first seen
+                        seqs[flow_interface[flow_id]].append((timestamp, seq_end, reinject_flow, conn_id))
+
+            # Now put all together on a same graph
+            offsets = {}
+            tot_offset = {co.WIFI: 0, co.CELL: 0}
+            seqs_plot = {co.WIFI: [], co.CELL: []}
+            for ith, seqs_ith in seqs.iteritems():
+                seqs_sort = sorted(seqs_ith, key=lambda elem: elem[0])
+                for elem in seqs_sort:
+                    if elem[3] not in offsets:
+                        offsets[elem[3]] = elem[1]
+                        seqs_plot[ith].append((elem[0], tot_offset[ith]))
+                    else:
+                        seqs_plot[ith].append((elem[0], tot_offset[ith] + (elem[1] - offsets[elem[3]])))
+                        tot_offset[ith] += elem[1] - offsets[elem[3]]
+                        offsets[elem[3]] = elem[1]
+
+            start_ts = min(seqs_plot[co.WIFI][0][0], seqs_plot[co.CELL][0][0])
+            fig, ax = plt.subplots()
+            ax.plot([x[0] for x in seqs_plot[co.WIFI]], [x[1] for x in seqs_plot[co.WIFI]], 'r-')
+            ax.plot([x[0] for x in seqs_plot[co.CELL]], [x[1] for x in seqs_plot[co.CELL]], 'b-')
+            plt.savefig(os.path.join(sums_dir_exp, fname + '.pdf'))
+
+        elif fname.startswith('tcp'):
+            for xpl_path in glob.glob(os.path.join(csv_dir_exp, fname + '_*.xpl')):
+                xpl_fname = os.path.basename(xpl_path)
+                # Preprocessing, avoid wasting time with not interesting files
+                conn_id, from_server_to_smartphone = tcp.get_flow_name(csv_fname)
+                if not from_server_to_smartphone:
+                    continue
+
+                # Opening of the file
+                try:
+                    csv_file = open(csv_path)
+                    data = csv_file.readlines()
+                    csv_file.close()
+                except IOError as e:
+                    print(str(e))
+                    continue
+
+                # Now process the file
+                conn = connections[fname][conn_id]
+                start_connections.append(conn.attr[co.START])
+                interface = conn.flow.attr[co.IF]
+                for line in data:
+                    if line.startswith("uarrow") or line.startswith("diamond"):
+                        split_line = line.split(" ")
+                        if ((not split_line[0] == "diamond") or (len(split_line) == 4 and "white" in split_line[3])):
+                            time = float(split_line[1])
+                            seqs[interface].append([time, int(split_line[2]), conn_id])
+
+            # Now put all togetger on a same graph
+            offsets = {}
+            tot_offset = {co.WIFI: 0, co.CELL: 0}
+            seqs_plot = {co.WIFI: [], co.CELL: []}
+            for ith, seqs_ith in seqs.iteritems():
+                seqs_sort = sorted(seqs_ith, key=lambda elem: elem[0])
+                for elem in seqs_sort:
+                    if elem[2] not in offsets:
+                        offsets[elem[2]] = elem[1]
+                        seqs_plot[ith].append((elem[0], tot_offset[ith]))
+                    else:
+                        seqs_plot[ith].append((elem[0], tot_offset[ith] + (elem[1] - offsets[elem[2]])))
+                        tot_offset[ith] += elem[1] - offsets[elem[2]]
+                        offsets[elem[2]] = elem[1]
+
+            start_ts = min(seqs_plot[co.WIFI][0][0], seqs_plot[co.CELL][0][0])
+            fig, ax = plt.subplots()
+            ax.plot([x[0] for x in seqs_plot[co.WIFI]], [x[1] for x in seqs_plot[co.WIFI]], 'r-')
+            ax.plot([x[0] for x in seqs_plot[co.CELL]], [x[1] for x in seqs_plot[co.CELL]], 'b-')
+            plt.savefig(os.path.join(sums_dir_exp, fname + '.pdf'))
+
+seq_d2s_all_connections()
+
 
 def collect_seq():
     seqs = {}
