114a678dba282cb6efe9feaa4621465623403231
Quentin De Coninck <quentindeco54@gmail.com>
2015-05-19 17:53:55 +0200

    [FIX] seq_together: fix some gliches on graphs + log another problem

--- seq_together.py
+++ seq_together.py
@@ -173,12 +173,12 @@ def seq_d2s_all_connections(time_loss=1.5):
                         split_line = line.split(" ")
                         # if ((not split_line[0] == "diamond") or (len(split_line) == 4 and "white" in split_line[3])):
                         time = float(split_line[1])
-                        seqs[interface].append([time + offset_duration[conn_id][flow_id], int(split_line[2]), flow_name])
                         if last_time and time > last_time + time_loss:
                             # Seems the connection has been broken
                             break
                         else:
                             last_time = time
+                        seqs[interface].append([time + offset_duration[conn_id][flow_id], int(split_line[2]), flow_name])
 
                 if last_time:
                     conn_event[interface].append((last_time, 'end'))
@@ -208,6 +208,7 @@ def seq_d2s_all_connections(time_loss=1.5):
             for ith, seqs_ith in seqs.iteritems():
                 # If needed, insert 0s in curves to show loss of connectivity
                 sorted_events = sorted(conn_event[interface], key=lambda elem: elem[0])
+                print(sorted_events)
                 counter = 0
                 sorted_event_plot = [(0.0, 0)]
                 for event_time, event in sorted_events:
@@ -313,12 +314,12 @@ def seq_d2s_all_connections(time_loss=1.5):
                     if line.startswith("uarrow") or line.startswith("diamond"):
                         split_line = line.split(" ")
                         time = float(split_line[1])
-                        seqs[interface].append([time, int(split_line[2]), conn_id])
                         if last_time and time > last_time + time_loss:
                             # Seems the connection has been broken
                             break
                         else:
                             last_time = time
+                        seqs[interface].append([time, int(split_line[2]), conn_id])
 
                 if last_time:
                     conn_event[interface].append((last_time, 'end'))
