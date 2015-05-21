ee4ed5fe21d8eba01c4753b882dafd70834d469e
Quentin De Coninck <quentindeco54@gmail.com>
2015-05-21 15:58:11 +0200

    [FIX] seq_together: still for this glitch

--- seq_together.py
+++ seq_together.py
@@ -372,8 +372,8 @@ def seq_d2s_all_connections(time_loss=1.5):
                     if event == 'start':
                         if counter == 0:
                             sorted_event_plot.append((event_time - 0.005000, 0))
-                            index = bisect.bisect_left(seqs_plot[ith], event_time - 0.004999)
-                            sorted_event_plot.append((event_time - 0.004999, seqs_plot[ith][index][1]))
+                            index = bisect.bisect_left(seqs_plot[ith], event_time - 0.003000)
+                            sorted_event_plot.append((event_time - 0.003000, seqs_plot[ith][index][1]))
                         counter += 1
                     elif event == 'end':
                         if counter == 0:
