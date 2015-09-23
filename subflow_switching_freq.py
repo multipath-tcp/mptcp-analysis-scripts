3a12910ff1252a8eb1d2ea240c610e6615102954
Quentin De Coninck <quentindeco54@gmail.com>
2015-09-23 13:14:20 +0200

    [FIX] subflow_switching_freq: check duration

--- subflow_switching_freq.py
+++ subflow_switching_freq.py
@@ -74,6 +74,7 @@ def plot(connections, multiflow_connections, sums_dir_exp):
     graph_fname_sec = "merge_bursts_sec_log"
     base_graph_path_sec = os.path.join(sums_dir_exp, graph_fname_sec)
     min_bytes = 1000000
+    min_duration = 0.1
 
     for fname, conns in multiflow_connections.iteritems():
         for conn_id, conn in conns.iteritems():
@@ -87,7 +88,7 @@ def plot(connections, multiflow_connections, sums_dir_exp):
                     continue
 
                 for direction in co.DIRECTIONS:
-                    if conn.attr[direction].get(co.BYTES_MPTCPTRACE, 0) < min_bytes:
+                    if conn.attr[direction].get(co.BYTES_MPTCPTRACE, 0) < min_bytes and conn.attr.get(co.DURATION, 0.0) <= min_duration:
                         continue
                     if co.BURSTS in conn.attr[direction] and len(conn.attr[direction][co.BURSTS]) > 0:
                         bursts_sec[direction].append((len(conn.attr[direction][co.BURSTS]) - 1.0) / conn.attr[co.DURATION])
